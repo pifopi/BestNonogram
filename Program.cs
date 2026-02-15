9237956780ef5791ac8492d7f14095657c73b146
pifopi <dottel.gael@gmail.com>
2026-02-15 20:24:12 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,197 @@
+﻿namespace BestNonogram
+{
+    enum PuzzleType { Color, BW }
+    enum PuzzleDifficulty { TrueNonogram, OtherNonogram }
+
+    abstract class PuzzleBase { }
+
+    class LastDonePuzzle : PuzzleBase
+    {
+        public required string Name { get; set; }
+        public required DateTime LastDone { get; set; }
+    }
+
+    sealed class LastDonePuzzleMap : CsvHelper.Configuration.ClassMap<LastDonePuzzle>
+    {
+        public LastDonePuzzleMap()
+        {
+            Map(m => m.Name).Name("Name");
+            Map(m => m.LastDone).Name("LastDone");
+        }
+    }
+
+    class Puzzle : PuzzleBase
+    {
+        public required string Name { get; set; }
+        public required int XP { get; set; }
+        public required int Size { get; set; }
+        public required PuzzleDifficulty Difficulty { get; set; }
+        public required PuzzleType Type { get; set; }
+        public required DateTime LastDone { get; set; }
+    }
+
+    sealed class PuzzleMap : CsvHelper.Configuration.ClassMap<Puzzle>
+    {
+        public PuzzleMap(PuzzleType puzzleType, List<LastDonePuzzle> lastDonePuzzles)
+        {
+            Map(m => m.Name).Name("Puzzle ID:Puzzle Name");
+            Map(m => m.XP).Convert(args =>
+            {
+                string? XP = args.Row.GetField(4);
+                System.Diagnostics.Debug.Assert(XP != null, "XP field is missing in the CSV");
+                return int.Parse(XP.Replace("~", ""));
+            });
+            Map(m => m.Size).Convert(args =>
+            {
+                string? size = args.Row.GetField("Size");
+                System.Diagnostics.Debug.Assert(size != null, "Size field is missing in the CSV");
+                string [] sizes = size.Split('x');
+                System.Diagnostics.Debug.Assert(sizes.Count() == 2, $"Size field is invalid {size}");
+                int width = int.Parse(sizes[0]);
+                int height = int.Parse(sizes[1]);
+                return int.Min(width, height);
+            });
+            Map(m => m.Difficulty).Convert(args =>
+            {
+                string? type = args.Row.GetField("Puzzle<br>type");
+                if (type is not null && type.Contains("True_nonogram_icon.png"))
+                {
+                    return PuzzleDifficulty.TrueNonogram;
+                }
+                else
+                {
+                    return PuzzleDifficulty.OtherNonogram;
+                }
+            });
+            Map(m => m.Type).Constant(puzzleType);
+            Map(m => m.LastDone).Convert(args =>
+            {
+                string? name = args.Row.GetField("Puzzle ID:Puzzle Name");
+                LastDonePuzzle? lastDonePuzzle = lastDonePuzzles.Find(p => p.Name == name);
+                return lastDonePuzzle != null ? lastDonePuzzle.LastDone : DateTime.MinValue;
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = "../../../config/";
+            string lastDonePuzzlesFile = Path.Combine(directory, "LastDonePuzzles.csv");
+            List<LastDonePuzzle> lastDonePuzzles = GetPuzzlesFromCsv<LastDonePuzzle>(lastDonePuzzlesFile, new LastDonePuzzleMap());
+
+            List<Puzzle> allColors = GetAllPuzzles(directory, PuzzleType.Color, lastDonePuzzles);
+            List<Puzzle> allBWs = GetAllPuzzles(directory, PuzzleType.BW, lastDonePuzzles);
+
+            while (true)
+            {
+                Console.WriteLine();
+                PrintBest("color (mortar)        ", FilterPuzzles(allColors  , withKatana: false,    filterNonTrueNonogram: false), allColors.Count);
+                PrintBest("color (katana)        ", FilterPuzzles(allColors  , withKatana: true,     filterNonTrueNonogram: false), allColors.Count);
+                PrintBest("B&W (mortar)          ", FilterPuzzles(allBWs     , withKatana: false,    filterNonTrueNonogram: false), allBWs.Count);
+                PrintBest("B&W (katana)          ", FilterPuzzles(allBWs     , withKatana: true,     filterNonTrueNonogram: false), allBWs.Count);
+                PrintBest("true nonogram (mortar)", FilterPuzzles(allBWs     , withKatana: false,    filterNonTrueNonogram: true),  allBWs.Count);
+                PrintBest("true nonogram (katana)", FilterPuzzles(allBWs     , withKatana: true,     filterNonTrueNonogram: true),  allBWs.Count);
+
+                Console.WriteLine("Enter Puzzle Name to mark as done:");
+                string? input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                List<Puzzle> allPuzzles = [..allColors, ..allBWs];
+                Puzzle? puzzle = allPuzzles.Find(p => p.Name == input);
+                if (puzzle is null)
+                {
+                    Console.WriteLine("Puzzle not found.");
+                }
+                else
+                {
+                    puzzle.LastDone = DateTime.Now;
+                    UpdateLastUsed(puzzle.Name, lastDonePuzzlesFile, lastDonePuzzles);
+                }
+            }
+        }
+
+        static List<Puzzle> GetAllPuzzles(string directory, PuzzleType puzzleType, List<LastDonePuzzle> lastDonePuzzles)
+        {
+            string file = puzzleType switch
+            {
+                PuzzleType.Color => "Colors.csv",
+                PuzzleType.BW => "BWs.csv",
+                _ => throw new Exception("Invalid puzzle type")
+            };
+            PuzzleMap map = new PuzzleMap(puzzleType, lastDonePuzzles);
+            return GetPuzzlesFromCsv<Puzzle>(Path.Combine(directory, file), map);
+        }
+
+        static List<T> GetPuzzlesFromCsv<T>(string csvFile, CsvHelper.Configuration.ClassMap map)
+            where T: PuzzleBase
+        {
+            Console.WriteLine($"Reading csv file : {csvFile}");
+
+            var config = new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture);
+            using (var reader = new StreamReader(csvFile))
+            using (var csv = new CsvHelper.CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap(map);
+                return csv.GetRecords<T>().ToList();
+            }
+        }
+
+        static List<Puzzle> FilterPuzzles(List<Puzzle> input, bool withKatana, bool filterNonTrueNonogram)
+        {
+            IEnumerable<Puzzle> puzzles = input.AsEnumerable();
+
+            DateTime cutoff = DateTime.Now.AddDays(-31);
+            puzzles = puzzles.Where(p => p.LastDone < cutoff);
+
+            if (withKatana)
+            {
+                puzzles = puzzles.OrderByDescending(p => p.XP / p.Size);
+            }
+            else
+            {
+                puzzles = puzzles.OrderByDescending(p => p.XP);
+            }
+
+            if (filterNonTrueNonogram)
+            {
+                puzzles = puzzles.Where(p => p.Difficulty == PuzzleDifficulty.TrueNonogram);
+            }
+            return puzzles.ToList();
+        }
+
+        static void UpdateLastUsed(string name, string lastDonePuzzlesFile, List<LastDonePuzzle> lastDonePuzzles)
+        {
+            LastDonePuzzle? lastUsed = lastDonePuzzles.Find(p => p.Name == name);
+            if (lastUsed == null)
+            {
+                lastDonePuzzles.Add(new LastDonePuzzle { Name = name, LastDone = DateTime.Now });
+            }
+            else
+            {
+                lastUsed.LastDone = DateTime.Now;
+            }
+            WritePuzzlesToCsv(lastDonePuzzlesFile, lastDonePuzzles);
+        }
+
+        static void WritePuzzlesToCsv(string lastDonePuzzlesFile, List<LastDonePuzzle> lastDonePuzzles)
+        {
+            var config = new CsvHelper.Configuration.CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture);
+            using (var writer = new StreamWriter(lastDonePuzzlesFile))
+            using (var csv = new CsvHelper.CsvWriter(writer, config))
+            {
+                csv.WriteRecords(lastDonePuzzles);
+            }
+        }
+
+        static void PrintBest(string label, List<Puzzle> filtered, int total)
+        {
+            string name = filtered.Count > 0 ? filtered[0].Name : "NONE";
+            Console.WriteLine($"Best {label} ({filtered.Count,4}/{total,4} left): {name}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Best Nonogram
+This C# program help you do the puzzles that will earns you the most XP on nonogram katana. It will filter out puzzles that you made in the last 31 days and sort them for XP. Here is a short usage guide
+
+- Copy paste the name of the puzzle you wish to solve into the game
+- Solve the nonogram
+- Enter the name of the puzzle you just did in the terminal
+- Press enter
+
+If you wish to update the list of puzzles, you can use https://addons.mozilla.org/en-US/firefox/addon/tbl2csv/ and do those steps : Right click -> "Table to CSV" -> "HTML content" for those two tables :
+- https://nonograms-katana.fandom.com/wiki/High_XP_puzzle_collection#Color_nonograms
+- https://nonograms-katana.fandom.com/wiki/High_XP_puzzle_collection#Black-and-white_nonograms
+
+Thanks to the nonoggram katana wiki for this amazing list of nonogram ! https://nonograms-katana.fandom.com/wiki
